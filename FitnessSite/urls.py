2e2dd599bdf1f78b3551164f0b8fd0fa3c3ca488
K1R1EIIIKA <sedoikin.k@gmail.com>
2023-11-23 02:30:44 +0300

    Rename app, add responsive styles

--- FitnessSite/urls.py
+++ FitnessSite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for TestSite project.
+URL configuration for FitnessSite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
